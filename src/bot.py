189a3571febd218e7f61e9fa1ea119ff47369504
heyitsaamir <ajawaid191@gmail.com>
2024-12-17 13:38:10 -0800

    Add confirm memorized fields

--- src/bot.py
+++ src/bot.py
@@ -8,11 +8,13 @@ from typing import List, Literal
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../packages"))
 
-from botbuilder.core import MemoryStorage, TurnContext
+from botbuilder.core import CardFactory, MemoryStorage, TurnContext
+from botbuilder.schema import Activity
 from litellm import acompletion
-from memory_module import LLMConfig, MemoryModule, MemoryModuleConfig, Message
+from memory_module import LLMConfig, Memory, MemoryModule, MemoryModuleConfig, Message
 from pydantic import BaseModel, Field
 from teams import Application, ApplicationOptions, TeamsAdapter
+from teams.ai.citations import AIEntity, Appearance, ClientCitation
 from teams.state import TurnState
 
 from config import Config
@@ -96,12 +98,18 @@ class UserDetail(BaseModel):
     model_config = {"json_schema_extra": {"additionalProperties": False}}
     field_name: str
     field_value: str
+    memory_ids: List[str] = Field(description="A list of memory IDs for the field")
 
 
 class ExecuteTask(BaseModel):
     model_config = {"json_schema_extra": {"additionalProperties": False}}
     succint_summary_of_issue: str
     user_details: List[UserDetail] = Field(description="A key value pair of the user's details")
+
+
+class ConfirmMemorizedFields(BaseModel):
+    model_config = {"json_schema_extra": {"additionalProperties": False}}
+    fields_to_confirm: List[UserDetail]
 
 
 async def get_candidate_tasks(candidate_tasks: GetCandidateTasks) -> str:
@@ -118,10 +126,92 @@ async def get_memorized_fields(fields_to_retrieve: GetMemorizedFields) -> str:
         print("---")
 
         if result:
-            empty_obj[query] = ", ".join([r.content for r in result])
+            empty_obj[query] = ", ".join([f"{r.id}. {r.content}" for r in result])
         else:
             empty_obj[query] = None
     return json.dumps(empty_obj)
+
+
+confirmation_card = {
+    "type": "AdaptiveCard",
+    "body": [],
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.6",
+}
+
+
+def build_confirmation_card(fields_to_confirm: List[UserDetail]):
+    confirmation_card_copy = confirmation_card.copy()
+    # databinding is not supported in the card, so we need to manually replace the values
+    facts = []
+    for field in fields_to_confirm:
+        facts.append({"title": field.field_name, "value": field.field_value})
+    confirmation_card_copy["body"].append({"type": "FactSet", "facts": facts})
+    return CardFactory.adaptive_card(confirmation_card_copy)
+
+
+async def confirm_memorized_fields(fields_to_confirm: ConfirmMemorizedFields, context: TurnContext) -> str:
+    print("Confirming memorized fields", fields_to_confirm)
+    flattened_memory_ids = [
+        memory_id for user_detail in fields_to_confirm.fields_to_confirm for memory_id in user_detail.memory_ids
+    ]
+    memories = await memory_module.get_memories(flattened_memory_ids)
+    # group memories by field name
+    user_details_with_memories: List[tuple[UserDetail, Memory | None]] = []
+    for user_detail in fields_to_confirm.fields_to_confirm:
+        memories_for_user_detail = [memory for memory in memories if str(memory.id) in user_detail.memory_ids]
+        # just take the first one into account for citation (for now)
+        user_details_with_memories.append(
+            (user_detail, memories_for_user_detail[0] if memories_for_user_detail else None)
+        )
+
+    cited_memories: List[Memory] = [memory for _, memory in user_details_with_memories if memory is not None]
+    messages_for_cited_memories = await memory_module.get_messages([memory.id for memory in cited_memories])
+    print("messages_for_cited_memories", messages_for_cited_memories)
+    memory_strs = []
+    citations: List[ClientCitation] = []
+    for user_detail, associated_memory in user_details_with_memories:
+        idx = len(citations) + 1
+        if associated_memory:
+            memory_strs.append(f"{user_detail.field_name}: {user_detail.field_value} [{idx}]")
+            associated_message = (
+                messages_for_cited_memories[associated_memory.id][0]
+                if associated_memory.id in messages_for_cited_memories
+                else None
+            )
+            citations.append(
+                ClientCitation(
+                    str(idx),
+                    Appearance(
+                        name=user_detail.field_name,
+                        abstract=associated_memory.content,
+                        url=associated_message.deep_link if associated_message else None,
+                    ),
+                )
+            )
+        else:
+            memory_strs.append(f"{user_detail.field_name}: {user_detail.field_value}")
+            continue
+
+    memory_details_str = "<br>".join([memory_str for memory_str in memory_strs])
+    ai_entity = AIEntity(
+        additional_type=["AIGeneratedContent"],
+        citation=citations,
+    )
+    activity_with_card_attachment = Activity(
+        type="message",
+        text=f"Can you confirm the following fields?<br>{memory_details_str}",
+        entities=[ai_entity] if ai_entity else None,
+    )
+    await context.send_activity(activity_with_card_attachment)
+    return json.dumps(fields_to_confirm.model_dump())
+
+
+async def send_string_message(context: TurnContext, message: str):
+    activity = Activity(
+        type="message", text=message, entities=[AIEntity(additional_type=["AIGeneratedContent"], citation=[])]
+    )
+    await context.send_activity(activity)
 
 
 async def execute_task(task_name: ExecuteTask, context: TurnContext) -> str:
@@ -139,7 +229,7 @@ Come up with a solution to the user's issue.
         messages=[{"role": "system", "content": system_prompt}],
         temperature=0.9,
     )
-    await context.send_activity(res.choices[0].message.content)
+    await send_string_message(context, res.choices[0].message.content)
     return res.choices[0].message.content
 
 
@@ -165,6 +255,14 @@ def get_available_functions():
         {
             "type": "function",
             "function": {
+                "name": "confirm_memorized_fields",
+                "description": "Confirm the fields that have been previously memorized",
+                "parameters": ConfirmMemorizedFields.schema(),
+            },
+        },
+        {
+            "type": "function",
+            "function": {
                 "name": "execute_task",
                 "description": "Execute a troubleshooting task",
                 "parameters": ExecuteTask.schema(),
@@ -172,6 +270,19 @@ def get_available_functions():
             },
         },
     ]
+
+
+def build_deep_link(context: TurnContext, message_id: str):
+    conversation_ref = TurnContext.get_conversation_reference(context.activity)
+    if conversation_ref.conversation and conversation_ref.conversation.is_group:
+        deeplink_conversation_id = conversation_ref.conversation.id
+    elif conversation_ref.user and conversation_ref.bot:
+        user_aad_object_id = conversation_ref.user.aad_object_id
+        bot_id = conversation_ref.bot.id.replace("28:", "")
+        deeplink_conversation_id = f"19:{user_aad_object_id}_{bot_id}@unq.gbl.spaces"
+    else:
+        return None
+    return f"https://teams.microsoft.com/l/message/{deeplink_conversation_id}/{message_id}?context=%7B%22contextType%22%3A%22chat%22%7D"
 
 
 async def add_message(
@@ -196,17 +307,6 @@ async def add_message(
         return False
     user_aad_object_id = conversation_ref_dict.user.aad_object_id
     message_id = str(uuid.uuid4()) if override_message_id else context.activity.id
-    print(
-        "Adding message",
-        Message(
-            id=message_id,
-            content=content,
-            author_id=user_aad_object_id,
-            conversation_ref=conversation_ref_dict.conversation.id,
-            created_at=created_at or datetime.datetime.now(datetime.timezone.utc),
-            is_assistant_message=is_assistant_message,
-        ),
-    )
     await memory_module.add_message(
         Message(
             id=message_id,
@@ -215,13 +315,14 @@ async def add_message(
             conversation_ref=conversation_ref_dict.conversation.id,
             created_at=created_at or datetime.datetime.now(datetime.timezone.utc),
             is_assistant_message=is_assistant_message,
+            deep_link=build_deep_link(context, context.activity.id),
         )
     )
 
 
 @bot_app.conversation_update("membersAdded")
 async def on_members_added(context: TurnContext, state: TurnState):
-    await context.send_activity("Hello! I'm your IT Support Assistant. How can I assist you today?")
+    await send_string_message(context, "Hello! I'm your IT Support Assistant. How can I assist you today?")
     await add_message(context, "Hello! I'm your IT Support Assistant. How can I assist you today?", True)
 
 
@@ -243,7 +344,8 @@ To identify tasks:
 Note: Step 2 - Gather necessary information for the selected task.
 To gather missing fields for the task:
     Step 2a: Use the "get_memorized_fields" function to check if any required fields are already known.
-    Step 2b (If necessary): For each missing field, prompt the user to provide the required information.
+    Step 2b (If necessary): Use the "confirm_memorized_fields" function to confirm the fields if they are already known. You should only call this if the user has alraedy previously provided this information.
+    Step 2c (If necessary): For each missing field, prompt the user to provide the required information.
 
 Note: Step 3 - Execute the task.
 To execute the selected task:
@@ -261,7 +363,7 @@ If the user ends the conversation, display "Thank you! Let me know if you need a
 
 <INSTRUCTIONS>
 Run the provided PROGRAM by executing each step.
-"""
+"""  # noqa E501
     messages = await memory_module.retrieve_chat_history(conversation_ref_dict.conversation.id, {"last_minutes": 1})
     print("messages", messages)
     llm_messages = [
@@ -293,7 +395,7 @@ Run the provided PROGRAM by executing each step.
 
         if message.tool_calls is None and message.content is not None:
             await add_message(context, message.content, True, datetime.datetime.now(datetime.timezone.utc), True)
-            await context.send_activity(message.content)
+            await send_string_message(context, message.content)
             break
         elif message.tool_calls is None and message.content is None:
             print("No tool calls and no content")
@@ -309,9 +411,14 @@ Run the provided PROGRAM by executing each step.
             elif function_name == "get_memorized_fields":
                 args = GetMemorizedFields.model_validate_json(function_args)
                 res = await get_memorized_fields(args)
+            elif function_name == "confirm_memorized_fields":
+                args = ConfirmMemorizedFields.model_validate_json(function_args)
+                res = await confirm_memorized_fields(args, context)
+                should_break = True
             elif function_name == "execute_task":
                 args = ExecuteTask.model_validate_json(function_args)
                 res = await execute_task(args, context)
+                should_break = True
             else:
                 res = None
 
