1d24aabc30efa934b256faea326fef6ad7fae331
lima4 <lima4+odspmdb@microsoft.com>
2024-12-20 16:47:59 -0800

    update based on Aamir's review 2

--- packages/memory_module/core/memory_core.py
+++ packages/memory_module/core/memory_core.py
@@ -189,10 +189,10 @@ class MemoryCore(BaseMemoryCore):
         system_message = f"""You are a semantic memory management agent. Your goal is to determine whether this new memory is duplicated with existing old memories.
 Considerations:
 - Time-based order: Each old memory has a creation time. Please take creation time into consideration.
-- Repeated behavior: If the new memory indicates a repeated action or behavior over a period of time, it should be added to reflect the pattern.
+- Repeated behavior: If the new memory indicates a repeated idea over a period of time, it should be added to reflect the pattern.
 Return value:
-- Add: add new memory to database while keep old memories
-- Ignore: ignore new memory
+- Add: add new memory while keep old memories
+- Ignore: indicates that this memory is similar to an older memory and should be ignored
 Here are the old memories:
 {old_memory_content}
 Here is the new memory:
