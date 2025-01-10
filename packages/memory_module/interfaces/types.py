6a099e81931cf4b7803e5c188bb9c9adc7fef4b3
Corina Gum <>
2025-01-10 13:39:32 -0800

    Revert limiting to 3.12 of Python

--- packages/memory_module/interfaces/types.py
+++ packages/memory_module/interfaces/types.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from datetime import datetime
 from enum import Enum
-from typing import ClassVar, List, Optional, Union
+from typing import ClassVar, List, Optional
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 
@@ -79,8 +79,8 @@ class AssistantMessage(AssistantMessageInput):
     model_config = ConfigDict(from_attributes=True)
 
 
-MessageInput = Union[InternalMessageInput, UserMessageInput, AssistantMessageInput]
-Message = Union[InternalMessage, UserMessage, AssistantMessage]
+MessageInput = InternalMessageInput | UserMessageInput | AssistantMessageInput
+Message = InternalMessage | UserMessage | AssistantMessage
 
 
 class MemoryAttribution(BaseModel):
